f0573ef6711ef18f12c91623530043a0abbe899f
Chavez Harris <codedbychavez@gmail>
2022-11-01 16:15:46 -0400

    integrated amplitude

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/configcat-labs/ab-testing-in-go-sample
 
 go 1.19
 
-require github.com/amplitude/analytics-go v0.0.11 // indirect
+require (
+	github.com/amplitude/analytics-go v0.0.11 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"html/template"
+	"github.com/configcat-labs/ab-testing-in-go-sample/ampli"
 )
 
 var homePageTemplate = template.Must(template.ParseFiles("index.html"))
@@ -13,10 +14,18 @@ func homePageHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func storePageHandler(w http.ResponseWriter, r *http.Request) {
+	ampli.Instance.PageView("user@example.com")
 	storePageTemplate.Execute(w, nil)
 }
 
 func main() {
+	ampli.Instance.Load(ampli.LoadOptions{
+    Environment: ampli.EnvironmentDevelopment,
+		Client: ampli.LoadClientOptions{
+			APIKey: "f0f6506c41bf8cc3a341cd4febf17293",
+		},
+})
+
 	port := "3000"
 
 	mux := http.NewServeMux()
